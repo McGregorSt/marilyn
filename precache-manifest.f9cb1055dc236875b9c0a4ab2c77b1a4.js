f823ac8258a5ae1223ffb1b0600854c61918e8c7
Grzegorz Stasiak <stasiak.gregor@gmail.com>
2019-11-25 12:03:52 +0100

    Updates

---
+++ precache-manifest.f9cb1055dc236875b9c0a4ab2c77b1a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce842bee80e2b910703983f7ed0234f2",
+    "url": "/marilyn/index.html"
+  },
+  {
+    "revision": "26f0aeee74ac289ed0eb",
+    "url": "/marilyn/static/css/main.4c75c90d.chunk.css"
+  },
+  {
+    "revision": "c0845bd9d2243d5d4981",
+    "url": "/marilyn/static/js/2.fb5aa1aa.chunk.js"
+  },
+  {
+    "revision": "26f0aeee74ac289ed0eb",
+    "url": "/marilyn/static/js/main.17b50615.chunk.js"
+  },
+  {
+    "revision": "54f4c85b10b8cebba660",
+    "url": "/marilyn/static/js/runtime-main.16b87c98.js"
+  },
+  {
+    "revision": "ca1dcdf177b27bdb52d8da24a219e02d",
+    "url": "/marilyn/static/media/logo4.ca1dcdf1.jfif"
+  },
+  {
+    "revision": "0150dc4053e9f9e77b7f64fad1f48b8f",
+    "url": "/marilyn/static/media/navPic.0150dc40.jpg"
+  }
+]);
